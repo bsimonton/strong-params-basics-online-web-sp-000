72e1db53fe1980b19f7373b8e88ece9e4d265ed6
Brian Simonton <bsimonton78@gmail.com>
2019-09-04 00:50:43 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,9 +25,10 @@ end
  
 private
  
-def post_params
-  params.require(:post).permit(:title, :description)
+def post_params(*args)
+  params.require(:post).permit(*args)
 end
+
 
 	def edit
 	  @post = Post.find(params[:id])
